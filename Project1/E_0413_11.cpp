d584c5dc2fd3c7f8d6c8e0f5894618d11a450c6e
user <user@PID-NOM9F4PQ18C>
2023-04-13 17:41:16 +0900

    초기 커밋.

---
+++ Project1/E_0413_11.cpp
@@ -0,0 +1,62 @@
+//#include <stdio.h>
+//
+//int getInt();
+//int add(int, int);
+//int Compare(int, int);
+//void Show(int);
+//void test();
+//
+//int main()
+//{
+//	int x1 = 0, y1 = 0;
+//
+//	x1 = getInt();
+//	y1 = getInt();
+//
+//	printf("%d + %d = %d\n", x1, y1, add(x1, y1));
+//
+//	Show(getInt());
+//	
+//	test();
+//
+//	return 0;
+//}
+//
+//int getInt()
+//{
+//	int i;
+//
+//	printf("숫자 입력 : ");
+//	scanf_s("%d", &i); getchar();
+//
+//	return i;
+//}
+//
+//int add(int a, int b)
+//{
+//	return a + b;
+//}
+//
+//void Show(int num)
+//{
+//	printf("%d\n", num);
+//}
+//
+//void test()
+//{
+//	printf("aaaaaaaaaaaaaaaaaa\n");
+//	printf("aaaaaaaaaaaaaaaaaa\n");
+//	printf("aaaaaaaaaaaaaaaaaa\n");
+//	printf("aaaaaaaaaaaaaaaaaa\n");
+//	printf("aaaaaaaaaaaaaaaaaa\n");
+//	printf("aaaaaaaaaaaaaaaaaa\n");
+//	printf("aaaaaaaaaaaaaaaaaa\n");
+//}
+//
+//int Compare(int a, int b)
+//{
+//	if (a > b)
+//		return a;
+//	else
+//		return b;
+//}
---
+++ Project1/E_0413_F2.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+
+int factorial(int);
+int Fibobacci(int);
+
+int main()
+{
+	int num = 0;
+
+	printf("숫자 입력 : ");
+
+	scanf_s("%d", &num);
+
+	printf("%d! == %d", num, factorial(num));
+
+	printf("\n\n");
+
+	Fibobacci(num);
+
+	return 0;
+}
+
+int factorial(int n)
+{
+	if (n == 0)
+		return 1;
+
+	return n * factorial(n - 1);
+}
+
+int  Fibobacci(int n)
+{
+	
+	int f1 = 0, f2 = 1, f3 = 0;
+
+	if (n == 1)
+		printf("%d", f1);
+	else
+		printf("%d %d", f1, f2);
+
+	for (int i = 2; i < n; i++)
+	{
+		f3 = f1 + f2;
+
+		
+		printf(" %d", f3);
+
+		f1 = f2;
+		f2 = f3;
+	}
+}
