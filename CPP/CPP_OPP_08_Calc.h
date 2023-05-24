5a997447a8eab045ddcda6d297661f3011bc3c87
Aueld <44721590+Aueld@users.noreply.github.com>
2023-05-24 17:21:06 +0900

    Create CPP_OPP_08_Calc.h

---
+++ CPP/CPP_OPP_08_Calc.h
@@ -0,0 +1,14 @@
+#pragma once
+class CPP_OPP_08_Calc // 헤더: 선언부
+{
+	int value;
+	
+	public:
+		CPP_OPP_08_Calc(int value);
+	
+		CPP_OPP_08_Calc& Add(int value);
+		CPP_OPP_08_Calc& Sub(int value);
+		CPP_OPP_08_Calc& Mul(int value);
+		CPP_OPP_08_Calc& Div(int value);
+		CPP_OPP_08_Calc& Mod(int value);
+};
