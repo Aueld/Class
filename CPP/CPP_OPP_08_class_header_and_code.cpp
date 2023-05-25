162f519d835a328c065149c43e0335219499001f
Aueld <44721590+Aueld@users.noreply.github.com>
2023-05-25 17:55:16 +0900

    Update CPP_OPP_08_class_header_and_code.cpp

--- CPP/CPP_OPP_08_class_header_and_code.cpp
+++ CPP/CPP_OPP_08_class_header_and_code.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-#include "CPP_OPP_08_Calc.h"
+#include "CPP_OOP_08_Calc.h"
 
 using namespace std;
 
 int main()
 {
-	CPP_OPP_08_Calc calc(10);
+	CPP_OOP_08_Calc calc(10);
 
 	calc.Add(5).Div(2).Mul(1);
 
