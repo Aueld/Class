9b34be4ef667c4b05fd96ab503342f56119f5fe6
Aueld <44721590+Aueld@users.noreply.github.com>
2023-06-16 17:35:11 +0900

    Create CPP_Inheritance_11_virtual_function_and_polymorphism.cpp

---
+++ CPP/CPP_Inheritance_11_virtual_function_and_polymorphism.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+using namespace std;
+
+class A
+{
+public:
+	void Print() { cout << "A" << endl; }
+	virtual void Print1() { cout << "A1" << endl; }
+	virtual void Print2() { cout << "A2" << endl; }
+	virtual void Print3() { cout << "A3" << endl; }
+	virtual void Print4() final { cout << "A4" << endl; }
+};
+
+class B : public A
+{
+public:
+	void Print() { cout << "B" << endl; }
+	void Print1() override { cout << "B1" << endl; }
+	void Print2() override { cout << "B2" << endl; }
+	void Print3() override final { cout << "B3" << endl; }
+	//void Print4() { cout << "B4" << endl; }
+};
+
+class C : public B
+{
+public:
+	void Print() { cout << "C" << endl; }
+	void Print1() override { cout << "C1" << endl; }
+	void Print2() override final { cout << "C2" << endl; }
+	//void Print3() override final { cout << "C3" << endl; }
+};
+
+class D : public C
+{
+public:
+	void Print() { cout << "D" << endl; }
+	void Print1() override { cout << "D1" << endl; }
+	//void Print2() override final { cout << "D2" << endl; }
+};
+
+int main()
+{
+	A a;
+	B b;
+	C c;
+	D d;
+
+	A& refB = b;
+	A& refC = c;
+	A& refD = d;
+
+	cout << "------------ A" << endl;
+	a.Print();
+	a.Print1();
+	a.Print2();
+	a.Print3();
+	a.Print4();
+	cout << endl;
+
+	cout << "------------ B" << endl;
+	refB.Print();
+	refB.Print1();
+	refB.Print2();
+	refB.Print3();
+	refB.Print4();
+	cout << endl;
+
+	cout << "------------ C" << endl;
+	refC.Print();
+	refC.Print1();
+	refC.Print2();
+	refC.Print3();
+	refC.Print4();
+	cout << endl;
+
+	cout << "------------ D" << endl;
+	refD.Print();
+	refD.Print1();
+	refD.Print2();
+	refD.Print3();
+	refD.Print4();
+	cout << endl;
+
+	return 0;
+}
