cbb1598ea76a2687b21db87356e920dfe2e43421
Aueld <44721590+Aueld@users.noreply.github.com>
2023-06-22 17:52:59 +0900

    Create CPP_Template_02_my_array.cpp

---
+++ CPP/CPP_Template_02_my_array.cpp
@@ -0,0 +1,22 @@
+#include "CPP_Template_02_my_array.h"
+// 2. 만약에 메인에서 사용하려면 메인에
+// #include "CPP_Template_02_my_array.cpp" 추가
+// 3. 또는 .inl 파일 사용
+
+//template<class T>
+//inline my_array<T>::my_array(int length)
+//	: length(length)
+//{
+//	arr = new T[length];
+//}
+//
+//template<class T>
+//inline my_array<T>::~my_array()
+//{
+//	if (arr != nullptr)
+//		delete[] arr;
+//}
+//
+//template<class T>
+//inline void my_array<T>::Print()
+//{}
