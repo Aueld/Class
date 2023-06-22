484d3b35a6167f4cd7e0cc9d399b86d5ade749a7
Aueld <44721590+Aueld@users.noreply.github.com>
2023-06-22 17:53:54 +0900

    Create CPP_Template_02_class_template.cpp

---
+++ CPP/CPP_Template_02_class_template.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+#include "CPP_Template_02_my_array.h"
+//#include "CPP_Template_02_my_array.cpp"
+#include "CPP_Template_02_my_array.inl"
+
+using namespace std;
+
+int main()
+{
+
+
+	return 0;
+}
