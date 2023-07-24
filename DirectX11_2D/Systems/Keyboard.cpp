21fc520c01314d9dbb1cac28f3966cba356309fe
Aueld <44721590+Aueld@users.noreply.github.com>
2023-07-24 19:42:41 +0900

    Create Keyboard.cpp

---
+++ DirectX11_2D/Systems/Keyboard.cpp
@@ -0,0 +1,47 @@
+#include "Framework.h"
+#include "Keyboard.h"
+
+
+void Keyboard::Update()
+{
+	// memcpy, keyState를 keyOldState로 넘김
+	memcpy(keyOldState, keyState, sizeof(keyOldState));
+
+	// 상태 초기화
+	ZeroMemory(keyState, sizeof(keyState));
+	ZeroMemory(keyMap, sizeof(keyMap));
+
+
+	GetKeyboardState(keyState);
+
+	for (DWORD i = 0; i < MAX_INPUT_KEY; i++)
+	{
+		byte key = keyState[i] & 0x80;	// 눌린지 &연산으로 판단
+		keyState[i] = key ? 1 : 0;
+
+		int oldState = keyOldState[i];
+		int state = keyState[i];
+
+		if (oldState == 0 && state == 1)
+			keyMap[i] = KEY_INPUT_STATUS_DOWN; //이전 0, 현재 1 - KeyDown
+		else if (oldState == 1 && state == 0)
+			keyMap[i] = KEY_INPUT_STATUS_UP; //이전 1, 현재 0 - KeyUp
+		else if (oldState == 1 && state == 1)
+			keyMap[i] = KEY_INPUT_STATUS_PRESS; //이전 1, 현재 1 - KeyPress
+		else
+			keyMap[i] = KEY_INPUT_STATUS_NONE;
+	}
+}
+
+Keyboard::Keyboard()
+{
+	// 쓰레기값이 있을 수 있으므로 초기화
+	ZeroMemory(keyState, sizeof(keyState));
+	ZeroMemory(keyOldState, sizeof(keyOldState));
+	ZeroMemory(keyMap, sizeof(keyMap));
+}
+
+Keyboard::~Keyboard()
+{
+
+}
