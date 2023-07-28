b27e43cfb42e8a34e78b60132280c65cdc513aa3
Aueld <44721590+Aueld@users.noreply.github.com>
2023-07-28 19:18:25 +0900

    Create VertexType.h

---
+++ DirectX11_2D/Renders/Resources/VertexType.h
@@ -0,0 +1,19 @@
+#pragma once
+
+struct VertexColor
+{
+	VertexColor()
+		: position(0, 0, 0), color(0, 0, 0, 0)
+	{}
+
+	VertexColor(Vector3 position, Color color)
+		: position(position), color(color)
+	{}
+
+	Vector3 position;
+	Color color;
+
+	static D3D11_INPUT_ELEMENT_DESC descs[];	// 데이터 자료
+	static const uint count = 2;
+
+};
