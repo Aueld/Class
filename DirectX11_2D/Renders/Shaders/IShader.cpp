99b7e60b700ecfb2b90b3f19c6b7fd9451806d6b
Aueld <44721590+Aueld@users.noreply.github.com>
2023-07-31 19:19:10 +0900

    Create IShader.cpp

---
+++ DirectX11_2D/Renders/Shaders/IShader.cpp
@@ -0,0 +1,43 @@
+#include "Framework.h"
+#include "IShader.h"
+
+// 셰이더 파일 컴파일, 오류 발생시 error Blob 생성
+void IShader::CompileShader(wstring path, string entryName, string profile, ID3DBlob** blob)
+{
+	ID3DBlob* error = nullptr;
+	HRESULT hr = D3DCompileFromFile
+	(
+		path.c_str(),
+		nullptr,
+		nullptr,
+		entryName.c_str(),
+		profile.c_str(),
+		D3DCOMPILE_ENABLE_STRICTNESS,
+		0,
+		blob,
+		&error
+	);
+
+	CheckShaderError(hr, error);
+	SAFE_RELEASE(error);
+}
+
+void IShader::CheckShaderError(HRESULT hr, ID3DBlob * error)
+{
+	if (FAILED(hr))
+	{
+		if (error)
+		{
+			string str = (const char*)error->GetBufferPointer();
+			MessageBoxA(handle, str.c_str(), "Shader Error", MB_OK);
+		}
+		CHECK(FALSE);
+	}
+}
+
+/*
+	ID3DBlob
+		메모리에 할당된 데이터 덩어리
+		셰이더 코드나 버퍼 데이터 등
+		메모리에 할당된 데이터 덩어리를 조작하고 정보를 추출할 수 있음
+*/
