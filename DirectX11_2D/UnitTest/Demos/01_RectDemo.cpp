0685384c9b7b4b710f951b9ee0ed91cd07853581
Aueld <44721590+Aueld@users.noreply.github.com>
2023-08-07 18:58:53 +0900

    Create 01_RectDemo.cpp

---
+++ DirectX11_2D/UnitTest/Demos/01_RectDemo.cpp
@@ -0,0 +1,34 @@
+#include "stdafx.h"
+#include "01_RectDemo.h"
+
+#include "Geometrics/Rect.h"
+
+void RectDemo::Init()
+{
+	rect = new Rect(Vector3(500, 200, 0), Vector3(100, 100, 0), 0.0f);
+}
+
+void RectDemo::Destroy()
+{
+	SAFE_DELETE(rect);
+}
+
+void RectDemo::Update()
+{
+	rect->Update();
+}
+
+void RectDemo::Render()
+{
+	rect->Render();
+}
+
+void RectDemo::PostRender()
+{
+
+}
+
+void RectDemo::GUI()
+{
+
+}
