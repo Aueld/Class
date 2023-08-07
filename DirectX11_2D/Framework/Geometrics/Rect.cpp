3c55e097931ddfce86e1d085c4b8a30da94b84ff
Aueld <44721590+Aueld@users.noreply.github.com>
2023-08-07 19:00:36 +0900

    Update Rect.cpp

--- DirectX11_2D/Framework/Geometrics/Rect.cpp
+++ DirectX11_2D/Framework/Geometrics/Rect.cpp
@@ -7,7 +7,7 @@ Rect::Rect(Vector3 position, Vector3 size, float rotation)
 	// 정점 버퍼 생성
 	vertices.assign(4, VertexColor());
 	vertices[0].position = Vector3(-0.5f, -0.5f, 0.0f);
-	vertices[1].position = Vector3-0.5f, 0.5f, 0.0f);
+	vertices[1].position = Vector3(-0.5f, 0.5f, 0.0f);
 	vertices[2].position = Vector3(0.5f, -0.5f, 0.0f);
 	vertices[3].position = Vector3(-0.5f, 0.5f, 0.0f);
 
@@ -20,7 +20,7 @@ Rect::Rect(Vector3 position, Vector3 size, float rotation)
 	vb->Create(vertices, D3D11_USAGE_DYNAMIC);
 
 	// 인덱스 버퍼 생성
-	indices = { 0,1,2 ,0,3,1 };
+	indices = { 0, 1, 2, 0, 3, 1 };
 	ib = new IndexBuffer();
 	ib->Create(indices, D3D11_USAGE_IMMUTABLE);
 
@@ -40,15 +40,44 @@ Rect::Rect(Vector3 position, Vector3 size, float rotation)
 
 Rect::~Rect()
 {
-
+	// 생성의 역순
+	SAFE_DELETE(wb);
+	SAFE_DELETE(ps);
+	SAFE_DELETE(il);
+	SAFE_DELETE(vs);
+	SAFE_DELETE(ib);
+	SAFE_DELETE(vb);
 }
 
 void Rect::Update()
 {
+	// Scale
+	D3DXMatrixScaling(&S, size.x, size.y, size.z);
+	// Rotation
+	D3DXMatrixRotationZ(&R, rotation);
+	// Translation (position)
+	D3DXMatrixTranslation(&T, position.x, position.y, position.z);
 
+	world = S * R * T;
+	wb->SetWorld(world);
 }
 
 void Rect::Render()
 {
+	// IA
+	vb->SetIA();
+	ib->SetIA();
+	il->SetIA();
+	DC->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+
+	// VS
+	vs->SetShader();
+	wb->SetVSBuffer(0);
+
+	// PS
+	ps->SetShader();
+
+	// OM
+	DC->DrawIndexed(ib->GetCount(), 0, 0);
 
 }
