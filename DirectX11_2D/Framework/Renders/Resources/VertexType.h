ce191fefa30dc6ba10eae91865e704e8afa8154e
Aueld <44721590+Aueld@users.noreply.github.com>
2023-08-09 20:35:02 +0900

    Update VertexType.h

--- DirectX11_2D/Framework/Renders/Resources/VertexType.h
+++ DirectX11_2D/Framework/Renders/Resources/VertexType.h
@@ -2,18 +2,32 @@
 
 struct VertexColor
 {
-	VertexColor()
-		: position(0, 0, 0), color(0, 0, 0, 0)
-	{}
+    VertexColor()
+        : position(0, 0, 0), color(0, 0, 0, 0) {}
 
-	VertexColor(Vector3 position, Color color)
-		: position(position), color(color)
-	{}
+    VertexColor(Vector3 position, Color color)
+        : position(position), color(color) {}
 
-	Vector3 position;
-	Color color;
+    Vector3 position;
+    Color color;
 
-	static D3D11_INPUT_ELEMENT_DESC descs[];	// 데이터 자료
-	static const uint count = 2;
+    static D3D11_INPUT_ELEMENT_DESC descs[];
+    static const uint count = 2;
+};
 
+struct VertexTexture
+{
+    VertexTexture()
+        : position(0, 0, 0), uv(0, 0)
+    {}
+
+    VertexTexture(Vector3 position, Vector2 uv)
+        : position(position), uv(uv)
+    {}
+
+    Vector3 position;
+    Vector2 uv;
+
+    static D3D11_INPUT_ELEMENT_DESC descs[];
+    static const uint count = 2;
 };
