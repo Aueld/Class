a8b90618314d8f2362e81acb297fa686067e9c8e
Aueld <44721590+Aueld@users.noreply.github.com>
2023-08-21 18:53:36 +0900

    Create Texture2D.cpp

---
+++ DirectX11_2D/Framework/Renders/Resources/Texture2D.cpp
@@ -0,0 +1,28 @@
+#include "Framework.h"
+#include "Texture2D.h"
+
+using namespace DirectX;
+vector<TextureDesc> Textures::descs;
+
+Texture2D::Texture2D(wstring filePath)
+{}
+
+void Texture2D::ReadPixel(vector<Color>*pixels)
+{}
+
+void Texture2D::ReadPixel(ID3D11Texture2D * texture, vector<Color>*pixels)
+{}
+
+void Textures::Delete()
+{}
+
+// 이미지 파일로부터 텍스처 데이터와 셰이더 리소스 뷰를 가지게 됨
+ID3D11ShaderResourceView* Textures::Load(Texture2D * texture)
+{
+	HRESULT hr;
+	TexMetadata metaData;
+
+
+
+	return nullptr;
+}
