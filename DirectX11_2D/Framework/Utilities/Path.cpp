9b87b396c060f286cd8ef3408ca5b2efaa02bcf4
Aueld <44721590+Aueld@users.noreply.github.com>
2023-08-21 19:45:10 +0900

    Update Path.cpp

--- DirectX11_2D/Framework/Utilities/Path.cpp
+++ DirectX11_2D/Framework/Utilities/Path.cpp
@@ -1,52 +1,91 @@
 #include "Framework.h"
 #include "Path.h"
 
+// 파일 존재 여부
 bool Path::ExistFile(string path)
 {
-	return false;
+	return ExistFile(String::ToWstring(path));
 }
 
 bool Path::ExistFile(wstring path)
 {
-	return false;
+	DWORD fileValue = GetFileAttributes(path.c_str());
+	return fileValue != INVALID_FILE_ATTRIBUTES;
 }
 
 bool Path::ExistDirectory(string path)
 {
-	return false;
+	return ExistDirectory(String::ToWstring(path));
 }
 
+// 디렉터리 존재 여부
 bool Path::ExistDirectory(wstring path)
 {
-	return false;
+	DWORD attribute = GetFileAttributes(path.c_str());
+
+	BOOL temp = (attribute != INVALID_FILE_ATTRIBUTES &&
+		attribute & FILE_ATTRIBUTE_DIRECTORY);
+
+	return temp == TRUE;
 }
 
 string Path::Combine(string path1, string path2)
 {
-	return string();
+	return path1 + path2;
 }
 
 wstring Path::Combine(wstring path1, wstring path2)
 {
-	return wstring();
+	return path1 + path2;
 }
 
-string Path::GetDirectoryName(string name)
+string Path::Combine(vector<string> paths)
 {
-	return string();
+	string temp = "";
+	for (string path : paths)
+		temp += path;
+
+	return temp;
 }
 
-wstring Path::GetDirectoryName(wstring name)
+wstring Path::Combine(vector<wstring> paths)
 {
-	return wstring();
+
+	wstring temp = L"";
+	for (wstring path : paths)
+		temp += path;
+
+	return temp;
+}
+
+string Path::GetDirectoryName(string path)
+{
+	String::Replace(&path, "\\", "/");
+	size_t index = path.find_last_of('/');
+
+	return path.substr(0, index + 1);
+}
+
+wstring Path::GetDirectoryName(wstring path)
+{
+	String::Replace(&path, L"\\", L"/");
+	size_t index = path.find_last_of('/');
+
+	return path.substr(0, index + 1);
 }
 
 string Path::GetExtension(string path)
 {
-	return string();
+	String::Replace(&path, "\\", "/");
+	size_t index = path.find_last_of('.');
+
+	return path.substr(index + 1, path.length());
 }
 
 wstring Path::GetExtension(wstring path)
 {
-	return wstring();
+	String::Replace(&path, L"\\", L"/");
+	size_t index = path.find_last_of('.');
+
+	return path.substr(index + 1, path.length());
 }
