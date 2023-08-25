6ea229b5d5671bbb0d657ebfd82e0fa2574434f4
Aueld <44721590+Aueld@users.noreply.github.com>
2023-08-25 20:24:16 +0900

    Update AnimationRect.cpp

--- DirectX11_2D/Framework/Geometrics/AnimationRect.cpp
+++ DirectX11_2D/Framework/Geometrics/AnimationRect.cpp
@@ -4,11 +4,12 @@
 AnimationRect::AnimationRect(Vector3 position, Vector3 size)
     : TextureRect(position, size, 0.0f)
 {
-    SetShader(ShaderPath + L"Animation.hlsl");
+    // 추 후 작업시 hlsl 파일 새로 만들어 사용 할 것
+    //SetShader(ShaderPath + L"Animation.hlsl");
 
     animator = new Animator;
 
-    Texture2D* srcTex = new Texture2D(TexturePath + L"player.png");
+    Texture2D* srcTex = new Texture2D(CharacterPath + L"player.png");
     AnimationClip* Run_R = new AnimationClip
     (
         L"Run_R", srcTex, 10,
@@ -29,13 +30,89 @@ AnimationRect::AnimationRect(Vector3 position, Vector3 size)
     animator->SetCurrentAnimClip(L"Run_R");
 
     SAFE_DELETE(srcTex);
+
+    // Sampler
+    {
+        D3D11_SAMPLER_DESC desc;
+        States::GetSamplerDesc(&desc);
+        States::CreateSampler(&desc, &point[0]);
+
+        desc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
+        States::CreateSampler(&desc, &point[1]);
+    }
+
+    // Blend
+    {
+        D3D11_BLEND_DESC desc;
+        States::GetBlendDesc(&desc);
+        States::CreateBlend(&desc, &bPoint[0]);
+
+        desc.RenderTarget[0].BlendEnable = true;
+        States::CreateBlend(&desc, &bPoint[1]);
+    }
+
 }
 
 AnimationRect::~AnimationRect()
 {}
 
 void AnimationRect::Update()
-{}
+{
+    animator->Update();
+
+    MapVertexBuffer();
+    {
+        Vector2 frame = animator->GetCurrentFrame();
+        Vector2 texelSize = animator->GetTexelFrameSize();
+        vertices[0].uv.x = frame.x;
+        vertices[0].uv.y = frame.y + texelSize.y;
+
+        vertices[1].uv.x = frame.x + texelSize.x;
+        vertices[1].uv.y = frame.y;
+
+        vertices[2].uv.x = frame.x + texelSize.x;
+        vertices[2].uv.y = frame.y + texelSize.y;
+
+        vertices[3].uv.x = frame.x;
+        vertices[3].uv.y = frame.y;
+    }
+    UnmapVertexBuffer();
+
+    __super::Update();
+}
 
 void AnimationRect::Render()
-{}
+{
+    srv = animator->GetCurrentSRV();
+
+    DC->PSSetSamplers(0, 1, &point[1]);
+    DC->OMSetBlendState(bPoint[1], nullptr, (UINT)0xFFFFFFFF);
+
+    __super::Render();
+}
+
+void AnimationRect::Move()
+{
+    auto* keyboard = Keyboard::Get();
+    float delta = Time::Delta();
+
+    if (keyboard->Press('W'))
+    {
+        position.y += 100 * delta;
+    }
+    if (keyboard->Press('S'))
+    {
+        position.y -= 100 * delta;
+
+    }
+    if (keyboard->Press('A'))
+    {
+        position.x -= 100 * delta;
+        animator->SetCurrentAnimClip(L"Run_L");
+    }
+    if (keyboard->Press('D'))
+    {
+        position.x += 100 * delta;
+        animator->SetCurrentAnimClip(L"Run_R");
+    }
+}
