85e6dc5153070a152e2e03f23b73fc074bb5ff90
Aueld <44721590+Aueld@users.noreply.github.com>
2023-08-28 19:48:48 +0900

    Update 07_AnimationDemo.cpp

--- DirectX11_2D/UnitTest/Demos/07_AnimationDemo.cpp
+++ DirectX11_2D/UnitTest/Demos/07_AnimationDemo.cpp
@@ -1,26 +1,31 @@
 #include "stdafx.h"
-#include "06_AnimationDemo.h"
+#include "07_AnimationDemo.h"
 
 #include "Geometries/AnimationRect.h"
+#include "Geometries/Player.h"
 
 void AnimationDemo::Init()
 {
-	ar = new AnimationRect({ 720, 360, 0 }, { 200, 200, 0 });
+	//ar = new AnimationRect({ 720, 360, 0 }, { 200, 200, 0 });
+	player = new Player({ 720, 360, 0 }, { 200, 200, 0 });
 }
 
 void AnimationDemo::Destroy()
 {
-	SAFE_DELETE(ar);
+	//SAFE_DELETE(ar);
+	SAFE_DELETE(player);
 }
 
 void AnimationDemo::Update()
 {
-	ar->Update();
+	//ar->Update();
+	player->Update();
 }
 
 void AnimationDemo::Render()
 {
-	ar->Render();
+	//ar->Render();
+	player->Render();
 }
 
 void AnimationDemo::PostRender()
