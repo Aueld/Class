58f064d392d8beacb83f90da23dfcf3409644e0e
Aueld <44721590+Aueld@users.noreply.github.com>
2023-08-28 19:47:18 +0900

    Create Player.h

---
+++ DirectX11_2D/Framework/Geometrics/Player.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "Geometries/AnimationRect.h"
+#include "Utilities/Animator.h"
+
+class Player
+{
+public:
+	Player(Vector3 position, Vector3 Size);
+	~Player();
+
+	void Update();
+	void Render();
+	void Move();
+
+private:
+	AnimationRect* animRect = nullptr;
+	Animator* animator = nullptr;
+};
