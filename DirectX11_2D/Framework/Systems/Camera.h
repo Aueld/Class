213d835676d9440cbcedf9d71e9a27057b66828a
Aueld <44721590+Aueld@users.noreply.github.com>
2023-09-01 18:49:36 +0900

    Create Camera.h

---
+++ DirectX11_2D/Framework/Systems/Camera.h
@@ -0,0 +1,31 @@
+#pragma once
+
+class Camera : public SingletonBase<Camera>
+{
+public:
+	friend SingletonBase<Camera>;
+
+	void Update();
+	void Render();
+	
+	void Move();
+	void UpdateView();
+
+	const Vector3 GetPosition() { return position; }
+
+	void SetPosition(Vector3 position) { this->position = position; }
+
+	void UnProjection(Vector3* out, Vector3 source, Matrix world = Values::Identity);
+
+private:
+	Camera();
+	~Camera();
+
+private:
+	VPBuffer* vpb = nullptr;
+	Matrix view, proj;
+
+	Vector3 position = Values::ZeroVec3;
+	float cameraSpeed = 100.0f;
+};
+
